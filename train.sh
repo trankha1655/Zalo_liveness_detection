55ba99f5cd85c51cf666ddc178c33168b55b4c0a
Tran Phan Nhat Kha <phamtruong7a1@gmail.com>
2022-11-05 23:56:45 +0700

    add train.sh

---
+++ train.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Deeplabv3plus_res101  PSPNet_res101  DualSeg_res101  BiSeNet  BiSeNetV2  DDRNet
+# FCN_ResNet  SegTrans
+
+python train.py --model Inception_Resnetv2  --max_epochs 300 --val_epochs 10 --batch_size 4 --lr 0.001  --root ..
