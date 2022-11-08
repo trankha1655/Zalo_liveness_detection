f2e225731b010a687e4603d526266691e764b14e
Tran Phan Nhat Kha <phamtruong7a1@gmail.com>
2022-11-08 13:39:16 +0700

    fix bug

--- train.sh
+++ train.sh
@@ -3,5 +3,5 @@
 # Deeplabv3plus_res101  PSPNet_res101  DualSeg_res101  BiSeNet  BiSeNetV2  DDRNet
 # FCN_ResNet  SegTrans
 
-python train.py --model MobileNetv2  --max_epochs 100 --val_epochs 5 --batch_size 64 --lr 0.001  \
+python train.py --model MobileNetv2  --max_epochs 150 --val_epochs 5 --batch_size 64 --lr 0.001  \
                     --root .. --resume ../Mobilenetv2_30epoch.pth --num_worker 4 
