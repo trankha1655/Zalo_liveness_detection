409d74e1b4bb5b2e9ec811275eef7012bfb9963e
Tran Phan Nhat Kha <phamtruong7a1@gmail.com>
2022-11-08 11:03:25 +0700

    fix bug

---
+++ install_requir.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ -e ../mobilenetv2.pth.tar ]
+then
+    echo "exist pretrained!"
+else
+    echo "downloading pretrained!"
+    gdown 14bHUOzvPlylAG2IJDy35vmj35jAN_oM9
+    mv mobilenetv2.pth.tar ../mobilenetv2.pth.tar
+fi
+if [ -e ../Mobilenetv2_30epoch.pth ]
+then
+    echo "exist weight!"
+else
+    echo "downloading best_weight!"
+    gdown 1iPk9eKZjY3yQhpEVicLYkNddX3FAgnSd
+    mv Mobilenetv2_20epoch.pth ../Mobilenetv2_20epoch.pth
+fi
+
--- train.sh
+++ train.sh
@@ -3,5 +3,5 @@
 # Deeplabv3plus_res101  PSPNet_res101  DualSeg_res101  BiSeNet  BiSeNetV2  DDRNet
 # FCN_ResNet  SegTrans
 
-python train.py --model MobileNetv2  --max_epochs 100 --val_epochs 10 --batch_size 64 --lr 0.001  \
+python train.py --model MobileNetv2  --max_epochs 100 --val_epochs 5 --batch_size 64 --lr 0.001  \
                     --root .. --resume ../Mobilenetv2_20epoch.pth --num_worker 4 --
